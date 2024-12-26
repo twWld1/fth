9c1d1ebb45c2e9068a74a9d4fd81989bb8a81da4
twWld1 <122292274+twWld1@users.noreply.github.com>
2024-12-26 22:56:43 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -1,1 +1,56 @@
-const _0xe3cb9f=_0x2022;(function(_0x1c10df,_0x193633){const _0x2428a1=_0x2022,_0x4fe073=_0x1c10df();while(!![]){try{const _0x236dfa=parseInt(_0x2428a1(0x153))/0x1+parseInt(_0x2428a1(0x160))/0x2*(parseInt(_0x2428a1(0x159))/0x3)+-parseInt(_0x2428a1(0x157))/0x4*(parseInt(_0x2428a1(0x168))/0x5)+parseInt(_0x2428a1(0x14f))/0x6*(parseInt(_0x2428a1(0x163))/0x7)+parseInt(_0x2428a1(0x158))/0x8+parseInt(_0x2428a1(0x14a))/0x9+-parseInt(_0x2428a1(0x154))/0xa;if(_0x236dfa===_0x193633)break;else _0x4fe073['push'](_0x4fe073['shift']());}catch(_0x13b119){_0x4fe073['push'](_0x4fe073['shift']());}}}(_0x6114,0xd2a48));function _0x2022(_0x4d718e,_0xbb8030){const _0x61141c=_0x6114();return _0x2022=function(_0x2022fd,_0x4f514e){_0x2022fd=_0x2022fd-0x145;let _0x4ee902=_0x61141c[_0x2022fd];return _0x4ee902;},_0x2022(_0x4d718e,_0xbb8030);}function _0x6114(){const _0x2455ed=['ANTI_LINK','MODE','\x0a┏━┫*⚬Lααɾα-ᴍᴅ-ꜱᴇᴛᴛɪɴɢꜱ⚬*┣━✾\x0a┃\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20*ᴸ\x20\x20ͣ\x20\x20ͣ\x20\x20ͬ\x20\x20ͣ\x20\x20✻\x20\x20ᴸ\x20\x20ͣ\x20\x20ͣ\x20\x20ͬ\x20\x20ͣ*\x0a┻\x0a*ᴍᴏᴅᴇ\x20➭*\x20_','../command','https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/daenerys%20targaryen.jpeg','_\x0a\x0a>\x20Lααɾα-ᴍᴅ\x20✻\x0a','_\x0a*ᴠᴏɪᴄᴇ_ʀᴇᴘʟʏ\x20➭*\x20_','7654086lsVVXH','_\x0a*ᴀᴜᴛᴏ_ʀᴇᴀᴅ_ᴍꜱɢ\x20➭*\x20_','sendMessage','READ_MESSAGE','https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg','9327804hCPjCc','AUTO_READ_STATUS','_\x0a*ᴀɴᴛɪ_ʙᴀᴅ_ᴡᴏʀᴅ\x20➭*\x20_','ꜱᴀᴅᴇᴇꜱʜᴀ\x20ᴛʜᴀʀᴜᴍɪɴ','300406lZcQMi','10735100XsokjJ','_\x0a*ꜱᴛɪᴄᴋᴇʀ_ʀᴇᴘʟʏ\x20➭*\x20_','setting','4ACvFCk','323776yUQQAE','4449VfPLII','AUTO_REPLY','_\x0a*ᴄʜᴀᴛ_ʀᴇᴘʟʏ\x20➭*\x20_','download','LARA\x20MD','log','94779062397@s.whatsapp.net','710WBkbMB','AUTO_STICKER','AUTO_REACT','7COcbgy','FAKE_RECORDING','_\x0a*ꜰᴀᴋᴇ_ʀᴇᴄᴏʀᴅɪɴɢ\x20➭*\x20_','_\x0a*ᴀɴᴛɪ_ʟɪɴᴋ\x20➭*\x20_','*❌\x20You\x20are\x20not\x20the\x20owner!*','6680645dTXxgf','settings','120363192254044294@newsletter','ANTI_BAD','../config','AUTO_VOICE'];_0x6114=function(){return _0x2455ed;};return _0x6114();}const {cmd,commands}=require(_0xe3cb9f(0x146)),config=require(_0xe3cb9f(0x16c));cmd({'pattern':_0xe3cb9f(0x169),'alias':_0xe3cb9f(0x156),'desc':'To\x20download\x20songs.','react':'⚙️','category':_0xe3cb9f(0x15c),'filename':__filename},async(_0x21a96d,_0x32ff4c,_0x42d636,{from:_0x225c0d,isOwner:_0x7d85f4,reply:_0x55b613,q:_0x1fdd6f})=>{const _0x12adeb=_0xe3cb9f;try{if(!_0x7d85f4)return _0x55b613(_0x12adeb(0x167));let _0x413a1a=_0x12adeb(0x145)+config[_0x12adeb(0x16f)]+_0x12adeb(0x15b)+config[_0x12adeb(0x15a)]+_0x12adeb(0x149)+config[_0x12adeb(0x16d)]+_0x12adeb(0x155)+config[_0x12adeb(0x161)]+'_\x0a*ᴀᴜᴛᴏ_ʀᴇᴀᴅ_ꜱᴛᴀᴛᴜꜱ\x20➭*\x20_'+config[_0x12adeb(0x150)]+'_\x0a*ᴀᴜᴛᴏ_ʀᴇᴀᴄᴛ\x20➭*\x20_'+config[_0x12adeb(0x162)]+_0x12adeb(0x151)+config[_0x12adeb(0x16b)]+_0x12adeb(0x166)+config[_0x12adeb(0x16e)]+_0x12adeb(0x14b)+config[_0x12adeb(0x14d)]+_0x12adeb(0x165)+config[_0x12adeb(0x164)]+_0x12adeb(0x148);return await _0x21a96d[_0x12adeb(0x14c)](_0x225c0d,{'image':{'url':_0x12adeb(0x147)},'caption':_0x413a1a,'contextInfo':{'mentionedJid':[_0x12adeb(0x15f)],'groupMentions':[],'forwardingScore':0x1,'isForwarded':!![],'forwardedNewsletterMessageInfo':{'newsletterJid':_0x12adeb(0x16a),'newsletterName':'Lααɾα-ᴍᴅ\x20✻','serverMessageId':0x3e7},'externalAdReply':{'title':_0x12adeb(0x15d),'body':_0x12adeb(0x152),'mediaType':0x1,'sourceUrl':'https://github.com/sadiyamin','thumbnailUrl':_0x12adeb(0x14e),'renderLargerThumbnail':![],'showAdAttribution':!![]}}},{'quoted':_0x32ff4c});}catch(_0x35bc16){console[_0x12adeb(0x15e)](_0x35bc16),_0x55b613(''+_0x35bc16);}});
+const {
+  cmd,
+  commands
+} = require("../command");
+const config = require("../config");
+cmd({
+  'pattern': "settings",
+  'alias': "setting",
+  'desc': "To download songs.",
+  'react': '⚙️',
+  'category': "download",
+  'filename': __filename
+}, async (_0x21a96d, _0x32ff4c, _0x42d636, {
+  from: _0x225c0d,
+  isOwner: _0x7d85f4,
+  reply: _0x55b613,
+  q: _0x1fdd6f
+}) => {
+  try {
+    if (!_0x7d85f4) {
+      return _0x55b613("*❌ You are not the owner!*");
+    }
+    let _0x413a1a = "\n┏━┫*⚬TWORLD-ᴍᴅ-ꜱᴇᴛᴛɪɴɢꜱ⚬*┣━✾\n┃            *ᴸ  ͣ  ͣ  ͬ  ͣ  ✻  ᴸ  ͣ  ͣ  ͬ  ͣ*\n┻\n*ᴍᴏᴅᴇ ➭* _" + config.MODE + "_\n*ᴄʜᴀᴛ_ʀᴇᴘʟʏ ➭* _" + config.AUTO_REPLY + "_\n*ᴠᴏɪᴄᴇ_ʀᴇᴘʟʏ ➭* _" + config.AUTO_VOICE + "_\n*ꜱᴛɪᴄᴋᴇʀ_ʀᴇᴘʟʏ ➭* _" + config.AUTO_STICKER + "_\n*ᴀᴜᴛᴏ_ʀᴇᴀᴅ_ꜱᴛᴀᴛᴜꜱ ➭* _" + config.AUTO_READ_STATUS + "_\n*ᴀᴜᴛᴏ_ʀᴇᴀᴄᴛ ➭* _" + config.AUTO_REACT + "_\n*ᴀɴᴛɪ_ʙᴀᴅ_ᴡᴏʀᴅ ➭* _" + config.ANTI_BAD + "_\n*ᴀɴᴛɪ_ʟɪɴᴋ ➭* _" + config.ANTI_LINK + "_\n*ᴀᴜᴛᴏ_ʀᴇᴀᴅ_ᴍꜱɢ ➭* _" + config.READ_MESSAGE + "_\n*ꜰᴀᴋᴇ_ʀᴇᴄᴏʀᴅɪɴɢ ➭* _" + config.FAKE_RECORDING + "_\n\n> TWORLD-ᴍᴅ ✻\n";
+    return await _0x21a96d.sendMessage(_0x225c0d, {
+      'image': {
+        'url': "https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/daenerys%20targaryen.jpeg"
+      },
+      'caption': _0x413a1a,
+      'contextInfo': {
+        'mentionedJid': ["94779062397@s.whatsapp.net"],
+        'groupMentions': [],
+        'forwardingScore': 0x1,
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363192254044294@newsletter",
+          'newsletterName': "TWORLD-ᴍᴅ ✻",
+          'serverMessageId': 0x3e7
+        },
+        'externalAdReply': {
+          'title': "TWORLD MD",
+          'body': "Techno World",
+          'mediaType': 0x1,
+          'sourceUrl': 'https://chat.whatsapp.com/DRbySkWgcUpEGVUkbsI6jt',
+          'thumbnailUrl': "https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg",
+          'renderLargerThumbnail': false,
+          'showAdAttribution': true
+        }
+      }
+    }, {
+      'quoted': _0x32ff4c
+    });
+  } catch (_0x35bc16) {
+    console.log(_0x35bc16);
+    _0x55b613('' + _0x35bc16);
+  }
+});
