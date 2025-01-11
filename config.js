04caac1ac36c201335247f564d07ed8d612ca12e
twWld1 <122292274+twWld1@users.noreply.github.com>
2025-01-11 10:35:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "fmoTxAhI#QveTjpYYOJcfil4MWXMz39KxNUAB4fRq
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
